090e5e62240df859f34254e86ddf72dbe0116d4e
Emerson Solano <43726865+Emerson93gh@users.noreply.github.com>
2020-10-04 10:09:56 -0600

    Corrigiendo enlaces
    Enlaces de dirrección entre páginas web.

--- modelos/VistasModelo.php
+++ modelos/VistasModelo.php
@@ -4,7 +4,12 @@ class VistasModelo {
     /* --- Modelo obtener vistas --- */
     protected static function obtenerVistasModelo($vistas){
         //lista blanca para la URL
-        $listaBlanca = ["home","client-list"];
+        $listaBlanca = ["client-list","client-new","client-search",
+            "client-update","company","home","item-list","item-new",
+            "item-search","item-update","reservation-list","reservation-new",
+            "reservation-pending","reservation-search","reservation-update",
+            "user-list","reservation-reservation","user-new","user-search",
+            "user-update"];
         if(in_array($vistas, $listaBlanca)){
             if(is_file("./vistas/contenidos/".$vistas."-view.php")){
                 $contenido="./vistas/contenidos/".$vistas."-view.php";
